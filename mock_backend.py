e3c09a2bcdb6ffe495df8bc098b0690b90a54e5e
karl5252 <karl.michalik@gmail.com>
2025-06-01 15:58:42 +0200

    added config
    TODO:
    make base user class abstract (ABC),
    add method to load tenant config
    make mock_backend a fully fledged simulator
    
    add tenant specific user classes to put in there user journeys to test
    
    update config to hold bpid, update config to hold cart items SKU codes for identification.

--- mock_backend.py
+++ mock_backend.py
@@ -3,12 +3,35 @@ import random
 import time
 from datetime import datetime
 
-from flask import Flask, send_from_directory, request, jsonify
-
-# )
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
-
+TENANT_CONFIGS = {
+    "slumberland": {
+        "error_rate": 0.3,  # 30% error rate
+        "latency_range": (0.4, 1.2),  # Latency between 0.4 and 1.2 seconds
+        "error_message": "Gamma crash",
+        "response_size": "large" # maybe in the future change to kb?
+    },
+    "wonderland": {
+        "error_rate": 0.05,  # 5% error rate
+        "latency_range": (0.2, 0.4),  # Latency between 0.2 and 0.4 seconds
+        "error_message": "Wunderland timeout",
+        "response_size": "medium"
+    },
+    "neverwinter": {
+        "error_rate": 0.1,  # 10% error rate
+        "latency_range": (0.05, 0.1),  # Latency between 0.05 and 0.1 seconds
+        "error_message": "Neverwinter service unavailable",
+        "response_size": "small"
+    },
+    "default": {
+        "error_rate": 0.01,  # ALMOST no errors for default tenant
+        "latency_range": (0.05, 0.1),  # Default latency range
+        "error_message": "Service error",
+        "response_size": "small"
+    }
+}
 
 @app.route("/", methods=["POST"])
 def graphql_handler():
